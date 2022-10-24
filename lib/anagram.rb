dc5bbea4f853f15d3229e34582e3263a048a6c99
+ <+>
2022-10-24 12:17:10 +0300

    Completed assignment

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+class Anagram
+    def initialize(word)
+        @word = word
+    end
+    def match(array)
+        new_array = []
+        array.map do |word|
+           if word.chars.sort == @word.chars.sort
+            new_array << word
+           end 
+        end
+        new_array
+    end
+end
